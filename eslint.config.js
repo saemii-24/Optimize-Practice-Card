73f9ce0b8db7e1317eb639f3ec483d585b3e6099
saemii-24 <hims2419@gmail.com>
2024-06-16 19:45:49 +0900

    Feat: eslint, prettier 설정

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    extends: ['react-app', 'react-app/jest', 'plugin:prettier/recommended'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]
