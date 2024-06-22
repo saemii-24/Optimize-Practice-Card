5d890e57b62291199c934427c69d181e455e7424
saemii-24 <hims2419@gmail.com>
2024-06-22 10:33:27 +0900

    Test: emotion styled 테스트

--- src/components/shared/Try.tsx
+++ src/components/shared/Try.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { css, SerializedStyles } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const MidTitle = styled.div`
+  font-weight: bold;
+  font-size: 10rem;
+  color: blue;
+`
 
 // Try 컴포넌트 정의
 const Try = ({
@@ -22,7 +29,12 @@ Try.Left = ({ children }: { children: React.ReactNode }) => {
 }
 
 Try.Mid = ({ children }: { children: React.ReactNode }) => {
-  return <div className="try-mid">{children}</div>
+  return (
+    <div className="try-mid">
+      <MidTitle>중간 타이틀</MidTitle>
+      {children}
+    </div>
+  )
 }
 
 Try.Right = ({ children }: { children: React.ReactNode }) => {
