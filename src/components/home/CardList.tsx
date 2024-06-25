d98f0bbd84781ca79dd6018fa47e71cfb64f2ccf
saemii-24 <hims2419@gmail.com>
2024-06-25 23:47:21 +0900

    Optimize: skeleton 적용

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -47,7 +47,7 @@ function CardList() {
       <InfiniteScroll
         dataLength={cards.length}
         hasMore={hasNextPage}
-        loader={<></>}
+        loader={<ListRow.Skeleton />}
         next={loadMore}
         scrollThreshold="100px"
       >
